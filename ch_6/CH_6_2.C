a97c310003192824b5176f95a6fdafdd7203eafe
omkhadela <125334655+omkhadela@users.noreply.github.com>
2023-02-20 09:46:45 +0530

    Add files via upload

---
+++ ch_6/CH_6_2.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+  main()
+{
+  int a;
+  clrscr();
+  printf("enter the value of a : ");
+  scanf("%d",&a);
+
+
+  if(a<10)
+  {
+   printf("Is given number is natural");
+  }
+  else
+  {
+   printf("Is given number isn't natural");
+
+  }
+  getch();
+
+}
